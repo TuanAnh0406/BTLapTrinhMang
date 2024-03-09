ee50471ccf5be5d7427cef3a203b62a15d1e6c11
TuanAnh0406 <93960234+TuanAnh0406@users.noreply.github.com>
2024-03-09 14:11:11 +0700

    Create BTVNT4.java

---
+++ BTVNT4/src/main/java/com/mycompany/btvnt4/BTVNT4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.btvnt4;
+
+/**
+ *
+ * @author DELL
+ */
+public class BTVNT4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
