0f48617d673d9299b789c6a1975bcbcdc34a3503
TuanAnh0406 <93960234+TuanAnh0406@users.noreply.github.com>
2024-03-09 14:11:13 +0700

    Create T4ReadData.java

---
+++ BTVNT4/src/main/java/com/mycompany/btvnt4/T4ReadData.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.btvnt4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class T4ReadData {
+    public static void main(String[] args) {        
+        try {
+            //Tao doi tuong url tu 1 dia chi
+            URL url=new URL("https://www.google.com");
+            //mo ket noi den url
+            URLConnection uRLConnection=url.openConnection();
+            //Doc du lieu tu ket noi
+            BufferedReader reader=new BufferedReader(
+            new InputStreamReader(uRLConnection.getInputStream()));
+            //Doc tung dong
+            String line;
+            while ((line=reader.readLine())!=null) {
+                System.out.println(line);//In ra luong du lieu doc duoc
+            }
+            //Dong luong
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
