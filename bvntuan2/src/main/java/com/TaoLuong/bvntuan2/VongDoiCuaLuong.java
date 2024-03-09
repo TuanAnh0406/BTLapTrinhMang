9ab418bea8b1b50b0d80350cafaf325ab537b0ea
TuanAnh0406 <93960234+TuanAnh0406@users.noreply.github.com>
2024-03-09 14:12:39 +0700

    Create VongDoiCuaLuong.java

---
+++ bvntuan2/src/main/java/com/TaoLuong/bvntuan2/VongDoiCuaLuong.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.TaoLuong.bvntuan2;
+
+/**
+ *
+ * @author DELL
+ */
+// Bài 3
+public class VongDoiCuaLuong {
+    public static void main(String[] args){
+        //tao 1 luong moi
+        Thread t1 = new Thread(new MyRunable1());
+        t1.start();
+        try{
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //Ngat luong
+        t1.interrupt();
+    }
+}
+class MyRunable1 implements Runnable{
+    @Override
+        public void run() {
+    System.out.println("Luong dang chay - Trang thai" + 
+            Thread.currentThread().getState());
+    try {
+        Thread.sleep(5000);
+    }   catch (Exception e) {
+        System.out.println("Luong bi giac ngu - Trang thai"
+                +Thread.currentThread().getState());
+}
+        System.out.println("Luong da ket thuc - Trang thai"
+                +Thread.currentThread().getState());
+}
+}
