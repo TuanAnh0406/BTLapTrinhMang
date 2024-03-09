35e00ed027eab8a7136a214e91aa80369c2ea6f0
TuanAnh0406 <93960234+TuanAnh0406@users.noreply.github.com>
2024-03-09 14:11:38 +0700

    Create T4URL.java

---
+++ BTVNT4/src/main/java/com/mycompany/btvnt4/T4URL.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.btvnt4;
+
+import java.net.URL;
+
+public class T4URL {
+    public static void main(String[] args) {
+        try {
+            //Tao doi tuong URL tu1 dia chi URL
+            //host?thamso1=giatri1&thamso2=giatri2
+            URL url=new URL("https://myaccount.google.com/?utm_source=OGB&utm_medium=app&pli=1");
+            //Phan tich thong tin tu URL
+            System.out.println("Protocol: "+url.getProtocol());
+            System.out.println("Host: "+url.getHost());
+            System.out.println("Port: "+url.getPort());
+            System.out.println("Path: "+url.getPath());
+            System.out.println("Truy van: "+url.getQuery());
+            System.out.println("Tham Chieu: "+url.getRef());
+        } catch (Exception e) {
+        }
+    }
+}
